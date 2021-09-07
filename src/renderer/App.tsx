cb884e1a4004b21b991e16bb1114f94138497c92
Marcelo Seiji <ikuta.marcelo@gmail.com>
2021-09-06 23:17:01 -0300

    Base os basics

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,41 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.global.css';
+import './App.global.scss';
 
-const Hello = () => {
+const Calculator = () => {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="baseCalculator">
+      <div>some text</div>
     </div>
   );
 };
@@ -44,7 +14,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Calculator} />
       </Switch>
     </Router>
   );
